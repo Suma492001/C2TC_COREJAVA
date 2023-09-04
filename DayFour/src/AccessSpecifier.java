f95b9d7d39b0d2c2e8d7358588a9dffdfa8f5c78
suma492001 <sumaurs1295@gmail.com>
2023-09-04 14:36:05 +0530

    First commit

---
+++ DayFour/src/AccessSpecifier.java
@@ -0,0 +1,10 @@
+
+public class AccessSpecifier {
+	//Public ,private .Protected and Default
+	//Facebook:
+	//Only me - Private
+	//Only to my friend -Default
+	//Only to my friends of friends -protected
+	//Public
+
+}
---
+++ DayFour/src/com/cg/packageone/TestOne.java
@@ -0,0 +1,12 @@
+package com.cg.packageone;
+
+public class TestOne {
+	public int id;
+	public void display() {
+		System.out.println("access Modifier");
+	}
+	public static void main(String[]args) {
+		TestOne 
+	}
+
+}
