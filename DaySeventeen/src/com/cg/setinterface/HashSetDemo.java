b75e95218fb6688a3d24a9eabe33631b98714027
suma492001 <sumaurs1295@gmail.com>
2023-10-05 11:48:30 +0530

    First Commit

---
+++ DaySeventeen/src/com/cg/setinterface/HashSetDemo.java
@@ -0,0 +1,21 @@
+package com.cg.setinterface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+	
+	static void addElements(Set<Integer> numberHashSet) {
+		numberHashSet.add(61);
+		numberHashSet.add(41);
+		numberHashSet.add(61);
+		numberHashSet.add(61);
+		
+	}
+
+	public static void main(String[] args) {
+		Set<Integer> numberHashSet=new HashSet<>();
+
+	}
+
+}
